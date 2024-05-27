b8f96cf54e40150b4956a31e504efe3f9650a8f5
tycheaierdoe <166589230+flyingdeer000000@users.noreply.github.com>
2024-05-27 14:22:13 +0800

    up

--- scripts/service/text_process.py
+++ scripts/service/text_process.py
@@ -1,3 +1,5 @@
+import os
+
 import chardet
 
 
@@ -7,7 +9,7 @@ def to_utf8(src_file, des_file, buffer_size=512 * 1024):
 
     # Detect the source file encoding
     with open(src_file, 'rb') as source:
-        rawdata = source.read(1024)
+        rawdata = source.read(512)
         result = chardet.detect(rawdata)
         source_encoding = result['encoding']
 
@@ -19,3 +21,26 @@ def to_utf8(src_file, des_file, buffer_size=512 * 1024):
                 if not chunk:
                     break
                 target.write(chunk)
+
+
+def split_file(
+        src_file, des_dir,
+        encoding='utf-8',
+        prefix="", suffix=".txt",
+        padding=6
+):
+    os.makedirs(des_dir, exist_ok=True)
+
+    if prefix is None or prefix == "":
+        prefix = os.path.splitext(os.path.basename(src_file))[0] + "_"
+
+    with open(src_file, mode='r', encoding=encoding) as file:
+        content = file.read()
+        chapters = content.split('\n\n')  # Split by double new lines
+
+    for i, chapter in enumerate(chapters):
+        # Pad the chapter number with zeros
+        padded_chapter_num = str(i + 1).zfill(padding)
+        output_file_path = os.path.join(des_dir, f'{prefix}{padded_chapter_num}{suffix}')
+        with open(output_file_path, 'w', encoding=encoding) as file:
+            file.write(chapter)
--- test.py
+++ test.py
@@ -1,30 +1,10 @@
-from scripts import util
+from scripts.service import text_process
 
 if __name__ == '__main__':
+    src_file = "D:/novel/cthulhu/lord_of_mystery.txt"
+    des_dir = "D:/novel/cthulhu/output"
 
-    def callback(file1, file2):
-        if file1 is not None:
-            print(file1)
-        if file2 is not None:
-            print(file2)
-        print("======")
-
-
-    src_dir = "D:/novel"
-    des_dir = "D:/output"
-    """
-    util.FileIO.walk(
-        src_dir,
-        callback_file=callback,
-        file_ext=".txt",
-        depth_limit=3
-    )
-    """
-
-    util.FileIO.walk_des(
-        src_dir=src_dir,
+    text_process.split_file(
+        src_file=src_file,
         des_dir=des_dir,
-        callback_dir=None,
-        callback_file=callback,
-        depth_limit=3
     )
