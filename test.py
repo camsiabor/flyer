a8cc93493e9216da24083ef83fa96d5787220b6e
tycheaierdoe <166589230+flyingdeer000000@users.noreply.github.com>
2024-07-03 00:40:41 +0800

    up

--- test.py
+++ test.py
@@ -1,10 +1,49 @@
-from scripts.service import text_process
+import os
+
+import webuiapi
+
+
+async def test_webuiapi():
+    os.makedirs('output', mode=0o777, exist_ok=True)
+
+    client = webuiapi.WebUIApi(
+        host='127.0.0.1',
+        port=30001,
+        sampler='Euler a',
+        steps=20,
+    )
+
+    model_target = ''
+    model_current = client.util_get_current_model()
+    if model_current != '':
+        client.util_set_model('')
+
+    result = await client.txt2img(
+
+        # prompt
+        prompt="1girl, solo",
+        negative_prompt='low quality, worst quality',
+        # sampler
+        sampler_name='Euler a',
+        steps=20,
+        cfg_scale=7,
+        seed=1003,
+        # latent image
+        width=1024,
+        height=1024,
+
+        # async
+        use_async=True,
+    )
+
+    print(result.info)
+
+    result.image.save('output/test.png')
+
+    print('done')
+
+
+
 
 if __name__ == '__main__':
-    src_file = "D:/novel/cthulhu/lord_of_mystery.txt"
-    des_dir = "D:/novel/cthulhu/output"
-
-    text_process.split_file(
-        src_file=src_file,
-        des_dir=des_dir,
-    )
+    test_webuiapi()
