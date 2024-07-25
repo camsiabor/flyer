6ff51224d8d71cec270b489d428b001b44a5bdbd
tycheaierdoe <166589230+flyingdeer000000@users.noreply.github.com>
2024-07-25 17:01:01 +0800

    up

--- config/sd/preset/0_posture/nsfw/stand.py
+++
@@ -1,9 +0,0 @@
-from config.common import PresetCommon
-
-
-def init(*_):
-    data = [
-        "standing sex",
-        "standing sex, split, leg up",
-    ]
-    return PresetCommon.to_array(data)
