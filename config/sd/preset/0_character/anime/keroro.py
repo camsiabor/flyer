f03296b134e3a594a7fe1994143d6a133cd2d2a6
tycheaierdoe <166589230+flyingdeer000000@users.noreply.github.com>
2024-07-29 15:09:21 +0800

    up

--- config/sd/preset/0_character/anime/keroro.py
+++ config/sd/preset/0_character/anime/keroro.py
@@ -2,33 +2,24 @@ from scripts.common.collection import Collection
 
 # zhuyuan =====================================================================================
 
-zhuyuan = {
-    'L': '<lora:Zhu_Yuan__ZZZ__Pony:0.7>',
-    'T': 'imtdzhuyuan',
+alisa_southerncross = {
+    'L': {
+        'XL': '<lora:alisa southerncross e1:0.8>',
+        'Pony': '',
+    },
+    'T': 'alisa southerncross',
     'C': """
-    black hair, red hair,two tone hair,multicolored hair,streaked hair,bangs,ponytail,red eyes,
-    cropped jacket, gloves, collared shirt, pants, necktie, bodysuit,uniform,police,
+    orange hair, twintails, long hair, hair ornament, 
+    fake animal ears, cat ears, 
+    red eyes, 
+    skirt, long sleeves, 
+    black pantyhose, 
     """,
-    'C01': """
-    black hair, red hair,two tone hair,multicolored hair,streaked hair,bangs,ponytail,red eyes,
-    cropped jacket, gloves, collared shirt, pants, bodysuit,uniform,police,
-    (green necktie:0.5), (blue jacket:0.5), 
-    """,
-
-    'L02': '<lora:zzz_zhuyuan_ponyXL:1>',
-    'T02': 'zhuyuan',
-    'C02': """
-    ponytail,streaked hair,black and red hair,
-    orange eyes,blue jacket, green necktie,
-    police uniform,,long sleeves,black vest,
-    white shirt,collared shirt,
-    black gloves,black pants, belt,
-    """
-
 }
 
 everyone = {
-    'zhuyuan': zhuyuan
+    'alisa': alisa_southerncross,
+    'alisa_southerncross': alisa_southerncross
 }
 
 
--- config/sd/preset/0_character/hoyoverse/genshin.py
+++ config/sd/preset/0_character/hoyoverse/genshin.py
@@ -2,29 +2,6 @@ from scripts.common.collection import Collection
 
 # zhuyuan =====================================================================================
 
-zhuyuan = {
-    'L': '<lora:Zhu_Yuan__ZZZ__Pony:0.7>',
-    'T': 'imtdzhuyuan',
-    'C': """
-    black hair, red hair,two tone hair,multicolored hair,streaked hair,bangs,ponytail,red eyes,
-    cropped jacket, gloves, collared shirt, pants, necktie, bodysuit,uniform,police,
-    """,
-    'C01': """
-    black hair, red hair,two tone hair,multicolored hair,streaked hair,bangs,ponytail,red eyes,
-    cropped jacket, gloves, collared shirt, pants, bodysuit,uniform,police,
-    (green necktie:0.5), (blue jacket:0.5), 
-    """,
-
-    'L02': '<lora:zzz_zhuyuan_ponyXL:1>',
-    'T02': 'zhuyuan',
-    'C02': """
-    ponytail,streaked hair,black and red hair,
-    orange eyes,blue jacket, green necktie,
-    police uniform,,long sleeves,black vest,
-    white shirt,collared shirt,
-    black gloves,black pants, belt,
-    """
-}
 
 clorinde = {
     'L': '<lora:clorinde_pony:0.9>,clorinde_(genshin_impact)',
@@ -34,14 +11,10 @@ clorinde = {
     pencil skirt,black jacket,fold-over boots,
     ascot,thigh strap,white shirt,underbust,blue cape
     """,
-
 }
 
-
-
-
 everyone = {
-    'zhuyuan': zhuyuan
+    'clorinde': clorinde
 }
 
 
