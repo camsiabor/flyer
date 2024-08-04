0fc2aadba6131ac814d3ee2014564adc4cd9485e
tycheaierdoe <166589230+flyingdeer000000@users.noreply.github.com>
2024-08-04 19:56:18 +0800

    up

--- config/sd/preset/0_character/anime/eva.py
+++ config/sd/preset/0_character/anime/eva.py
@@ -18,15 +18,17 @@ asuka = {
 rei = {
     'C': {
         'App': """
-        ayanami rei, rebuild of evangelion,
-        blue hair, short hair, headgear,
-        red eyes, (multicolored eyes:${rand|0.0~0.5}),
+        (ayanami rei:1.05), (rebuild of evangelion:1.05),
+        (blue hair:0.9), short hair,
+        (red eyes:0.9), (multicolored eyes:${rand|0.0~0.35}),
         """,
         'Plugsuit': """        
-        plugsuit, (blue plugsuit:${rand|0.1~0.5}), (white plugsuit:${rand|0.25~0.75}),        
+        (headgear:0.95),
+        bodysuit, plugsuit, (blue plugsuit:${rand|0.1~0.5}), (white plugsuit:${rand|0.25~0.75}),        
         """,
         'PlugSuitBlack': """
-        plugsuit, (black plugsuit:${rand|0.33~0.75}),        
+        (headgear:0.95),
+        bodysuit, plugsuit, (black plugsuit:${rand|0.33~0.75}),        
         """,
         'School': """
         school uniform, (tokyo-3 middle school uniform:${rand|0.25~0.5}),
